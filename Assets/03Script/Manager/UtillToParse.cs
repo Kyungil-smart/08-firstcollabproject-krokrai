0a917466962d8428c4d4308753091682d399eb29
krokrai <mks3060@naver.com>
2026-03-26 14:16:06 +0900

    feat : UtillToParse에 bool 타입 반환 추가

--- Assets/03Script/Manager/UtillToParse.cs
+++ Assets/03Script/Manager/UtillToParse.cs
@@ -33,4 +33,29 @@ public static class UtillToParse
             i = -1;
         }
     }
+
+    public static void TryParseForSO(this string s, out bool i, in string objname = "지정된 경로 없음")
+    {
+        if (s != null)
+        {
+            switch(s)
+            {
+                case "FALSE":
+                    i = false;
+                    return;
+                case "TRUE":
+                    i = true;
+                    return;
+                default:
+                    Debug.LogWarning($"{objname}에 들러온 문자열이 유효하지 않은 값입니다. false를 반환합니다.");
+                    i = false;
+                    return;
+            }
+        }
+        else
+        {
+            Debug.LogWarning($"{objname}에 들러온 문자열이 유효하지 않은 값입니다. false를 반환합니다.");
+            i = false;
+        }
+    }
 }
