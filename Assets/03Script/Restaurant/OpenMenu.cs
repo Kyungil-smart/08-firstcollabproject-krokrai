be763c894c5639315c2f5d7e14de671e3084bc1b
krokrai <mks3060@naver.com>
2026-03-30 17:43:08 +0900

    fix : main Scene 버그 수정
    1. 도감 UI
    2. Trade UI
    3. 낚시

--- Assets/03Script/Restaurant/OpenMenu.cs
+++ Assets/03Script/Restaurant/OpenMenu.cs
@@ -5,6 +5,7 @@ public class OpenMenu : MonoBehaviour
 {
     [SerializeField] Button _btn;
     [SerializeField] GameObject _menu;
+    [SerializeField] GameObject _Rast;
     [SerializeField] GameObject _Fish;
 
     bool _active = false;
@@ -18,10 +19,17 @@ public class OpenMenu : MonoBehaviour
     public void ChangeFishScene()
     {
         _Fish.SetActive(true);
-        gameObject.SetActive(false);
+        _menu.SetActive(false);
+        _Rast.SetActive(false);
     }
 
     public void OnChanged()
+    {
+        _Rast.SetActive(true);
+        _Fish.SetActive(false);
+    }
+
+    public void OpenMenuPanel()
     {
         _active = !_active;
         _menu.SetActive(_active);
