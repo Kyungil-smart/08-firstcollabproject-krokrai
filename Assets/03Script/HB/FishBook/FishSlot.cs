6048805c8fc8e4b76e505042367f26fc439fb8b4
Nocdamso <adsnmf@gmail.com>
2026-04-01 03:03:28 +0900

    Refactor: 어드레서블 스크립트추가
    이미지 로드하는 어드레서블 스크립터블 따로 뺐습니다.
    빼면서 연동되어있던 코드들 구조 변경

--- Assets/03Script/HB/FishBook/FishSlot.cs
+++ Assets/03Script/HB/FishBook/FishSlot.cs
@@ -2,13 +2,11 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using UnityEngine.EventSystems;
-using UnityEngine.AddressableAssets;
-using UnityEngine.ResourceManagement.AsyncOperations;
 
 public class FishSlot : MonoBehaviour
 {
     [Header("원본 프리팹")]
-    public Image fishImage;     // 물고기 이미지
+    public AddressableImageLoader imageLoader;     // 물고기 이미지
     public FishListManager fishListManager; // 도감 매니저 참조용
 
     [Header("상점 전용 프리팹(도감용에선 비워둠)")]
@@ -19,19 +17,21 @@ public class FishSlot : MonoBehaviour
     private FishData _currentFishData;
     private TraderUI _traderUI;
 
-    // 로드된 이미지의 관리, 출력
-    private AsyncOperationHandle<Sprite> _handle;
 
     // 상점용 셋업 함수
     public void SetupTrader(FishData data, TraderUI trader)
     {
+        if (data == null) return;
+
         _currentFishData = data;
         _traderUI = trader;
 
-        if (fishImage != null)
-        {
-            LoadImageAsync(data.fishSprite);
-        }
+        // 상점UI 활성화
+        priceText?.gameObject.SetActive(true);
+        slotToggle?.gameObject.SetActive(true);
+
+        // 이미지 로드
+        imageLoader?.LoadImage(data?.fishSprite);
 
         // priceText가 연결돼 있을 때 실행(상점 전용)
         if (priceText != null)
@@ -47,54 +47,16 @@ public class FishSlot : MonoBehaviour
         {
             slotToggle.onValueChanged.RemoveAllListeners();
             slotToggle.onValueChanged.AddListener(OnToggleChanged);
+            slotToggle.isOn = false;
         }
     }
 
-    private void LoadImageAsync(string address)
-    {
-        // 주소가 없거나 "NullException"이면 돌아가기
-        if (string.IsNullOrEmpty(address) || address == "NullException")
-        {
-            Debug.Log("이미지 주소가 비었습니다.");
-            return;
-        }
-
-        // 로드된 이미지가 있다면, 불러오기 전 이미지를 메모리에서 해제
-        if (_handle.IsValid())
-        {
-            Addressables.Release(_handle);
-        }
-
-        // 구글 시트에 있는 이미지를 가져옴(결과물은 지금 X, _handle 이라는 영수증만 우선)
-        _handle = Addressables.LoadAssetAsync<Sprite>(address);
-
-        _handle.Completed += (operation) =>
-        {
-            if (operation.Status == AsyncOperationStatus.Succeeded)
-            {
-                fishImage.sprite = operation.Result;
-
-                fishImage.preserveAspect = true;
-            }
-        };
-    }
-
-    public void OnPointerClick(PointerEventData eventData)
-    {
-        // 도감용 FishSlot일 때만 동작
-        if(fishListManager != null && _currentFishData != null)
-        {
-            // FishList 오브젝트에 데이터 전달, UI갱신
-            fishListManager.ChangeFishData(_currentFishData);
-
-            // FishList 오브젝트 활성화
-            fishListManager.gameObject.SetActive(true);
-        }
-    }
 
     // 도감용 셋업 함수
     public void SetupFishBook(FishData data, FishListManager fishListManager)
     {
+        if (data == null) return;
+
         _currentFishData = data;
         this.fishListManager = fishListManager;
 
@@ -104,14 +66,9 @@ public class FishSlot : MonoBehaviour
         if (slotToggle != null) slotToggle.gameObject.SetActive(false);
         if (highlightUI != null) highlightUI.SetActive(false);
 
-        if (fishImage != null)
-        {
-            // 불러올 이미지 주소(string) 
-            string targetAddress = data.isCaught ? data.fishSprite : data.silhouetteSprite;
+        string spriteLoad = data.isCaught ? data.fishSprite : data.silhouetteSprite;
 
-            // 이미지 로드
-            LoadImageAsync(targetAddress);
-        }
+        imageLoader?.LoadImage(spriteLoad);
     }
 
     public void OnSlotClick()
@@ -129,28 +86,15 @@ public class FishSlot : MonoBehaviour
 
     private void OnToggleChanged(bool isOn)
     {
-        if (highlightUI != null)
-        {
-            highlightUI.SetActive(isOn);
-        }
+        highlightUI?.SetActive(isOn);
 
-        if(_traderUI != null)
-        {
-            _traderUI.OnSlotChanged();
-        }
+        // _traderUI가 연결되어 있을 때만 실행
+        _traderUI?.OnSlotChanged();
     }
 
     // 현재 물고기 데이터 반환 함수
     public FishData GetFishData()
     {
         return _currentFishData;
-    }
-
-    private void OnDestroy()
-    {
-        if (_handle.IsValid())
-        {
-            Addressables.Release(_handle);
-        }    
     }
 }
