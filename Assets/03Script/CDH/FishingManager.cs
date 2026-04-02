3640c2eff6ad61aca857e0b50a88a4d5726a8d0e
abcbbq99 <kim3373613@gmail.com>
2026-04-02 11:45:30 +0900

    feat : 터치 잠금 시스템 개발 및 모듈화
    프리팹 가져다가 쓰시면 되고 자세한 사용법은 노션에 정리할게요

--- Assets/03Script/CDH/FishingManager.cs
+++ Assets/03Script/CDH/FishingManager.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography;
-using UnityEditor.Experimental.GraphView;
+//using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
