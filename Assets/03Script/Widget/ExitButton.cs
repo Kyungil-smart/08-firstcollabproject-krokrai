f455fe232313c76205304a3f1dc59621eeb89398
krokrai <mks3060@naver.com>
2026-04-08 18:55:14 +0900

    fix #152 : 게임 종료 팝업창 생성 및 적용

--- Assets/03Script/Widget/ExitButton.cs
+++ Assets/03Script/Widget/ExitButton.cs
@@ -1,7 +1,32 @@
+using TMPro;
 using UnityEngine;
 
 public class ExitButton : MonoBehaviour
 {
+    [SerializeField] TextMeshProUGUI _exitText;
+    [SerializeField] TextMeshProUGUI _cancelText;
+    [SerializeField] TextMeshProUGUI _confirmText;
+
+    Language lang;
+
+    private void OnEnable()
+    {
+        lang = DataTower.instance.languageSetting;
+        _exitText.text = lang == Language.KOR ? "" : "Do you want to <color=red>exit</color> the game?";
+        _cancelText.text = lang == Language.KOR ? "아니요" : "No";
+        _confirmText.text = lang == Language.KOR ? "네" : "Yes";
+    }
+
+    public void OnExitPopup()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void Cancel()
+    {
+        gameObject.SetActive(false);
+    }
+
     public void OnExitGame()
     {
         Application.Quit();
