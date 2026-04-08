2f6f64f6b6f882a7099b8b2d66385955a9b9666c
krokrai <mks3060@naver.com>
2026-04-08 19:08:46 +0900

    fix : 데이터 기반 텍스트 출력으로 수정

--- Assets/03Script/Widget/ExitButton.cs
+++ Assets/03Script/Widget/ExitButton.cs
@@ -6,15 +6,25 @@ public class ExitButton : MonoBehaviour
     [SerializeField] TextMeshProUGUI _exitText;
     [SerializeField] TextMeshProUGUI _cancelText;
     [SerializeField] TextMeshProUGUI _confirmText;
+    [SerializeField] TranslationDataReader _reader;
 
     Language lang;
+
+    TranslationData _exitData;
+    TranslationData _cancelData;
+    TranslationData _confirmData;
 
     private void OnEnable()
     {
         lang = DataTower.instance.languageSetting;
-        _exitText.text = lang == Language.KOR ? "" : "Do you want to <color=red>exit</color> the game?";
-        _cancelText.text = lang == Language.KOR ? "아니요" : "No";
-        _confirmText.text = lang == Language.KOR ? "네" : "Yes";
+
+        _exitData = _reader.GetTranslationData("Game Exit");
+        _cancelData = _reader.GetTranslationData("No");
+        _confirmData = _reader.GetTranslationData("Yes");
+
+        _exitText.text = lang == Language.KOR ? _exitData.Kor : _exitData.En;
+        _cancelText.text = lang == Language.KOR ? _cancelData.Kor : _cancelData.En;
+        _confirmText.text = lang == Language.KOR ? _confirmData.Kor : _confirmData.En;
     }
 
     public void OnExitPopup()
